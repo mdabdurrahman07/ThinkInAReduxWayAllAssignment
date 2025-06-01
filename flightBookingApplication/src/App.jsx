b83ea371ef5b21b78cebc058a7ff14b174501a83
mdabdurrahman07 <jamilrahman2001@gmail.com>
2025-06-02 00:44:27 +0600

    flight Booking App preview integrated

--- flightBookingApplication/src/App.jsx
+++ flightBookingApplication/src/App.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import Header from './components/Header/Header';
 import InputCard from './components/InputCard/InputCard';
+import OutputCard from './components/OutputCard/OutputCard';
 
 const App = () => {
   return (
@@ -9,6 +10,8 @@ const App = () => {
       <Header/>
       {/* Input Section */}
       <InputCard/>
+      {/* Output Section */}
+      <OutputCard/>
     </main>
   );
 };
