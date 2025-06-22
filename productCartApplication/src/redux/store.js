0275d5dd61f23966b90853e563a5f4cbaad4b468
mdabdurrahman07 <jamilrahman2001@gmail.com>
2025-06-23 00:13:20 +0600

    redux dev tools dependencis added

--- productCartApplication/src/redux/store.js
+++ productCartApplication/src/redux/store.js
@@ -1,6 +1,7 @@
 import { createStore } from "redux";
 import combineReducer from "./rootReducer.js";
+import { composeWithDevTools } from "redux-devtools-extension";
 
-const store = createStore(combineReducer);
+const store = createStore(combineReducer, composeWithDevTools());
 
 export default store;
