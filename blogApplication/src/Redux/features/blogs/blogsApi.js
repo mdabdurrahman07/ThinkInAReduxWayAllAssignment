5fb83831a1002767bf756ad2ab003ad47007ff6e
mdabdurrahman07 <jamilrahman2001@gmail.com>
2025-07-22 01:15:05 +0600

    getblog api created

--- blogApplication/src/Redux/features/blogs/blogsApi.js
+++ blogApplication/src/Redux/features/blogs/blogsApi.js
@@ -2,5 +2,5 @@ import axios from "../../../utils/axios"
 
 export const getBlogs = async () =>{
     const response = await axios.get("/blogs") 
-    return response?.data
+    return await response?.data
 }
