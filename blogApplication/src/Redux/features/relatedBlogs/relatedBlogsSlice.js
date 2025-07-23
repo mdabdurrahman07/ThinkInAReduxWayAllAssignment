270089aaa903150f5cb2069f10414168cdcab69f
mdabdurrahman07 <jamilrahman2001@gmail.com>
2025-07-23 22:06:05 +0600

    relatedBlogs apis & slice implemented

---
+++ blogApplication/src/Redux/features/relatedBlogs/relatedBlogsSlice.js
@@ -0,0 +1,50 @@
+import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
+import { getRelatedBlogs } from "./relatedBlogsApi";
+
+// declaring the initialState
+
+const initialState = {
+  relatedBlog: [],
+  isLoading: false,
+  isError: false,
+  error: "",
+};
+
+// declaring the async thunk
+
+export const fetchRelatedBlogs = createAsyncThunk(
+  "relatedBlogs/fetchRelatedBlogs",
+  async ({ tags, id }) => {
+    const relatedBlogs = await getRelatedBlogs({ tags, id });
+    return relatedBlogs;
+  }
+);
+
+// declaring the reducer
+
+const relatedBlogsSlice = createSlice({
+  name: "relatedBlogs",
+  initialState,
+  extraReducers: (builder) => {
+    builder
+      .addCase(fetchRelatedBlogs.pending, (state) => {
+        (state.isError = false), (state.isLoading = true);
+      })
+      .addCase(fetchRelatedBlogs.fulfilled, (state, action) => {
+        state.isLoading = true;
+        state.relatedBlog = action.payload;
+      })
+      .addCase(fetchRelatedBlogs.fulfilled, (state, action) => {
+        state.isLoading = true;
+        state.relatedBlog = action.payload;
+      })
+      .addCase(fetchRelatedBlogs.rejected, (state, action) => {
+        state.isLoading = false;
+        state.relatedBlog = [];
+        state.isError = true;
+        state.error = action.error?.message;
+      });
+  },
+});
+
+export default relatedBlogsSlice.reducer;
