f4b00d91cee9ec196cef9b262701b8c236778c4c
mdabdurrahman07 <jamilrahman2001@gmail.com>
2025-07-31 23:56:45 +0600

    conditional title showing

--- jobFindingApplication/src/components/Header/Sorting.jsx
+++ jobFindingApplication/src/components/Header/Sorting.jsx
@@ -23,12 +23,12 @@ const Sorting = () => {
      <Job key={job?.id} jobs={job}/>
     ));
   }
-
+  const {types} = useSelector((state) => state.filters)
   return (
     <div className="lg:pl-[14rem]  mt-[5.8125rem]">
       <main className="max-w-3xl rounded-lg  mx-auto relative z-20 p-10 xl:max-w-none bg-[#1E293B]">
         <div className="md:flex space-y-2 md:space-y-0 justify-between mb-10 ">
-          <h1 className="lws-section-title">All Available Jobs</h1>
+          <h1 className="lws-section-title">{types === "" || types === undefined ? "All Available Jobs" : types}</h1>
           <div className="flex gap-4">
             <div className="search-field group flex-1">
               <i className="fa-solid fa-magnifying-glass search-icon group-focus-within:text-blue-500"></i>
